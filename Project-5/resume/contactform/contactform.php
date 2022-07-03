6d4b57b72cf25b06929c1d9ca7c841cc95ff327b
royalnavghans <93833578+royalnavghans@users.noreply.github.com>
2022-07-03 11:31:33 +0530

    .

---
+++ Project-5/resume/contactform/contactform.php
@@ -0,0 +1,8 @@
+<form>
+<h2>Contact us</h2>
+<p><label>Your Name</label> <input name="myEmail" type="text" /></p>
+<p><label>Email Address</label> <input style="cursor: pointer;" name="myEmail" type="text" /></p>
+<p><label>subject</label> <input name="subject" type="text" /></p>
+<p><label>Message</label>  <textarea name="message"></textarea> </p>
+<p><input type="submit" value="Send" /></p>
+</form>
