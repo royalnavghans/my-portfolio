76462a0fe2e1a8ad13e12a1cc170c1fabc5148b8
royalnavghans <93833578+royalnavghans@users.noreply.github.com>
2022-08-21 14:02:54 +0530

    .

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web-Developer", "Cloud Administrator","Technical/Cloud Supporter","Videographer", "Youtuber"],
+        strings: ["Full Stack Developer", "Cloud Administrator","Technical/Cloud Supporter","Videographer", "Youtuber"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web-Developer", "Cloud Administrator", "Technical/Cloud Supporter","Videographer", "Youtuber"],
+        strings: ["Full Stack Developer", "Cloud Administrator", "Technical/Cloud Supporter","Videographer", "Youtuber"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
